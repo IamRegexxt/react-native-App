4e43a57e39e88eca499752d1840489024c9f0342
Jesreel Amorganda <jeamorganda@gbox.ncf.edu.ph>
2024-05-21 08:04:38 +0800

    added label icons

--- navigation/MainDrawer.js
+++ navigation/MainDrawer.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack'; 
 import HomeScreen from './ScreenComponents/HomeScreen';
 import AboutScreen from './ScreenComponents/AboutScreen';
 import SkillsScreen from './ScreenComponents/SkillScreen';
 import EducationScreen from './ScreenComponents/EducationScreen';
 import ContactScreen from './ScreenComponents/ContactScreen';
-import LogoutButton from './ScreenComponents/Logout';
+import { Icon } from 'react-native-paper';
+
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
+
 
 const HomeStack = () => {
   return (
@@ -24,25 +26,70 @@ const HomeStack = () => {
 };
 
 // Main Drawer Navigator
-const MainDrawer = ({ navigation }) => {
+const MainDrawer = () => {
   return (
     <Drawer.Navigator>
       <Drawer.Screen 
         name="HomeStack" 
         component={HomeStack} 
-        options={{ drawerLabel: 'Home' }}
+        options={{ drawerLabel: 'Home',
+        drawerIcon: ({color, size})=>{
+          <Icon name="home" color={color} size={size}/>
+        }
+        }}
+        
       />
-      <Drawer.Screen name="About" component={AboutScreen} />
-      <Drawer.Screen name="Education" component={EducationScreen} />
-      <Drawer.Screen name="Skills" component={SkillsScreen} />
-      <Drawer.Screen name="Contact" component={ContactScreen} />
-      <Drawer.Screen
-        name="Logout"
-        component={() => <LogoutButton navigation={navigation} />}
-        options={{ drawerLabel: 'Logout' }}
+      <Drawer.Screen 
+        name="About" 
+        component={AboutScreen} 
+        options={{ 
+          drawerLabel: 'About',
+          drawerIcon: ({ color, size }) => (
+            <Icon name="info" color={color} size={size} />
+          ),
+        }} 
+      />
+      <Drawer.Screen 
+        name="Education" 
+        component={EducationScreen} 
+        options={{ 
+          drawerLabel: 'Education',
+          drawerIcon: ({ color, size }) => (
+            <Icon name="school" color={color} size={size} />
+          ),
+        }} 
+      />
+      <Drawer.Screen 
+        name="Skills" 
+        component={SkillsScreen} 
+        options={{ 
+          drawerLabel: 'Skills',
+          drawerIcon: ({ color, size }) => (
+            <Icon name="build" color={color} size={size} />
+          ),
+        }} 
+      />
+      <Drawer.Screen 
+        name="Contact" 
+        component={ContactScreen} 
+        options={{ 
+          drawerLabel: 'Contact',
+          drawerIcon: ({ color, size }) => (
+            <Icon name="contact-mail" color={color} size={size} />
+          ),
+        }} 
       />
     </Drawer.Navigator>
   );
 };
 
-export default MainDrawer;
+{/* <Stack.Screen
+  name="Home"
+  component={HomeScreen}
+  options={{
+    headerTitle: "Personal Website",
+    headerRight: () => (<Button title="Logout" onPress={handleLogout} />), // Add a logout button
+  }}
+/> */}
+
+export default MainDrawer; 
